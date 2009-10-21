9c531dce419a7e4ca21c74cc27a7bb608c64ee2d
ferran <none@none>
2009-10-21 17:39:51 +0200

    [svn r14] Add COPYRIGHT file and header on source files
    --HG--
    branch : cbl2pg

--- trunk/cbl2pg.h
+++ trunk/cbl2pg.h
@@ -1,3 +1,20 @@
+/*
+    Copyright (C) 2009  Ferran Pegueroles <ferran@pegueroles.com>
+    This file is part of cbl2pg.
+
+    cbl2pg is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    cbl2pg is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef _DLL_H_
 #define _DLL_H_
 
